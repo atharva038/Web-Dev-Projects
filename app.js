bb1b5ee950015e79963c0ec6f008b3dd64f2fffa
atharva038 <152613272+atharva038@users.noreply.github.com>
2024-07-28 14:43:18 +0530

    Calculator

---
+++ app.js
@@ -0,0 +1,39 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+
+
+Array.from(buttons).forEach((button) => {
+ 
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+          try{
+                string = eval(string);
+                document.querySelector("input").value = string;
+        }catch(error){
+            document.querySelector("input").value = "ERROR";
+            string = "";
+        }
+     
+    }
+    else if (e.target.innerHTML == "AC") {
+        try{
+            string = "";
+            document.querySelector("input").value = string;
+        }
+       
+      catch(error){
+        document.querySelector("input").value = "ERROR";
+        string = "";
+    }
+}
+else if(e.target.innerHTML == "X"){
+    string.replace("*");
+    document.querySelector("input").value = string;
+}
+    else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
